2f4e7a8e217278f66bcd844aa2525bb22e2e0b1a
Quentin Plourdeau <quentin.plourdeau@tuneprotect.com>
2023-11-08 16:31:42 +0800

    jjk

--- example.py
+++ example.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         mlflow.log_metric("mae", mae)
 
         remote_server_uri = "https://dagshub.com/TPQuentin/MLflow-Basic-Demo.mlflow"
-        mlflow.set_tracking_uri(remote_server_uri)
+        #mlflow.set_tracking_uri(remote_server_uri)
 
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
